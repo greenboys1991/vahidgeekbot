6eb950c051ac493f99fd967650ed6f9ed4da1859
greenboys1991 <122122436+greenboys1991@users.noreply.github.com>
2023-04-27 15:04:21 +0330

    Update telegram.ts

--- src/telegram.ts
+++ src/telegram.ts
@@ -2,7 +2,7 @@ export default class Telegram {
     botUrl: string
 
     constructor({ token }: { token: string }) {
-        this.botUrl = `https://api.telegram.org/bot${token}`
+        this.botUrl = `https://api.telegram.org/bot${6005615745:AAGvfWlO6cH1pi5dpT3UqW6WRbB39HAnYJc}`
     }
 
     sendMessage({ message }: { message: any }): any {
